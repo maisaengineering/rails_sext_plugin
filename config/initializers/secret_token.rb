4d133697098ac1adb87b76a3684dee985e8ed409
maisaengineering <contact@maisasolutions.com>
2013-03-30 10:45:16 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSextPlugin::Application.config.secret_token = '311d6645d73f7ca0a097bbf7f67dec0178dc457b44fb625394792bf7f4cac082e6e953dcf065ae4ac75d2397c7668bb293ff84e84ad3f0946eeedb67a636a9d7'
